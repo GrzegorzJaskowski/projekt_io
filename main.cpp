aa7643963136b9ea6f0b189fbdb686c7133636e9
ChrisJ102 <97360071+ChrisJ102@users.noreply.github.com>
2022-01-20 16:00:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,3 +6,26 @@ int main()
 {
     wczytywanie();
 }
+
+//PRZYKŁAD REFAKTORYZACJI FUNKCJI
+/*
+funkcja przed zmianą:
+void wypisanie(Gracz tab[]) {
+    int i = 0;
+    while (tab[i].login != "domyslne") {
+        std::cout << "[" << i << "] " << tab[i].login << " " << tab[i].haslo << " " << tab[i].wynik << " " << tab[i].rola_admin << " " << tab[i].dostep_do_konta << std::endl;
+        i++;
+    }
+}
+
+funkcja po zmianie:
+void wypisanie(Gracz tab[]) {
+    int i = 0;
+    while (tab[i].login != "domyslne") {
+        std::cout << "[" << i << "] " << tab[i].login << " " << tab[i].haslo
+            << " " << tab[i].wynik << " " << tab[i].rola_admin
+            << " " << tab[i].dostep_do_konta << std::endl;
+        i++;
+    }
+}
+*/
